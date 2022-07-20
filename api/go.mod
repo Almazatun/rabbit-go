71ce34d01bc382015a03053bf7fd3ec19b28c233
Almazatun <atunstyle@gmail.com>
2022-07-20 18:20:43 +0600

    delete gorilla mux

--- api/go.mod
+++ api/go.mod
@@ -2,7 +2,4 @@ module github.com/Almazatun/rabbit-go/api
 
 go 1.18
 
-require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/rabbitmq/amqp091-go v1.3.4 // indirect
-)
+require github.com/rabbitmq/amqp091-go v1.3.4
--- api/main.go
+++ api/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
-
 func main() {
-	go RabbitMQRPC()
-
-	router := mux.NewRouter()
-
-	log.Fatal(http.ListenAndServe(":3002", router))
-
+	RabbitMQRPC()
 }
